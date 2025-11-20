8294b07ed09b8a3836f5def491661985975144ab
denied0 <133315314+Acce5sDenied@users.noreply.github.com>
2025-11-20 07:47:38 -0100

    Update TNTCannonSim.js
    removed airtime cuz useless

--- Programs/TNTCannonSim/TNTCannonSim.js
+++ Programs/TNTCannonSim/TNTCannonSim.js
@@ -2,13 +2,11 @@
 Simulation for finding total distance traveled with a variable initial velocity.
 */
 
-let airtime, dist = [], pos, vel, absvel;
+let dist = [], pos, vel, absvel;
 for (let init = 0; init <= 1000; init++) {
   pos = { x:0, y:0, z:0 };
   vel = { x:init, y:0, z:0 };
-  airtime = 0;
   while (vel.x + vel.z > 0.0001) {
-    airtime += 1;
     absvel = Math.sqrt(vel.y ** 2 + Math.sqrt(vel.x ** 2 + vel.z ** 2) ** 2);
     if (absvel > 16) {
       pos.x += (vel.x / absvel * 16);
@@ -25,6 +23,6 @@ for (let init = 0; init <= 1000; init++) {
     vel.y *= 0.98;
     vel.z *= 0.91;
   }
-  dist.push({ Init:init, x:pos.x, y:pos.y, z:pos.z, ticks:airtime });
+  dist.push({ Init:init, x:pos.x, y:pos.y, z:pos.z });
 }
 console.table(dist);
